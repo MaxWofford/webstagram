e34398e841dc4405d8c00d71888cec60e3e14e4c
Max Wofford <max@maxwofford.com>
2015-12-20 01:03:32 -0800

    Load external css and js files

--- webstagram.js
+++ webstagram.js
@@ -1,5 +1,8 @@
+var filters = ['ig-xpro2', 'ig-willow', 'ig-walden', 'ig-valencia', 'ig-toaster', 'ig-sutro', 'ig-sierra', 'ig-rise', 'ig-nashville', 'ig-mayfair', 'ig-lofi', 'ig-kelvin', 'ig-inkwell', 'ig-hudson', 'ig-earlybird', 'ig-brannan', 'ig-amaro', 'ig-1977'];
+
 var els = document.getElementsByTagName('img');
 for (var i = 0; i < els.length; i++) {
   var el = els[i];
-  el.style.filter = "contrast(1.3) brightness(0.8) sepia(0.3) saturate(1.5) hue-rotate(-20deg)";
+  var randomFilter = filters[Math.round(Math.random() * (filters.length - 1))];
+  el.className += (' ' + randomFilter);
 }
