12334313da0dbfe98d0fcc6acfb85353b70ec3d5
Max Wofford <max@maxwofford.com>
2015-12-20 11:39:30 -0800

    Fix loading of external css and js

--- webstagram.js
+++ webstagram.js
@@ -1,8 +1,11 @@
-var filters = ['ig-xpro2', 'ig-willow', 'ig-walden', 'ig-valencia', 'ig-toaster', 'ig-sutro', 'ig-sierra', 'ig-rise', 'ig-nashville', 'ig-mayfair', 'ig-lofi', 'ig-kelvin', 'ig-inkwell', 'ig-hudson', 'ig-earlybird', 'ig-brannan', 'ig-amaro', 'ig-1977'];
+var filters = ['ig-xpro2', 'ig-willow', 'ig-walden', 'ig-valencia',
+'ig-toaster', 'ig-sutro', 'ig-sierra', 'ig-rise', 'ig-nashville', 'ig-mayfair',
+'ig-lofi', 'ig-kelvin', 'ig-inkwell', 'ig-hudson', 'ig-earlybird', 'ig-brannan',
+'ig-amaro', 'ig-1977'];
 
 var els = document.getElementsByTagName('img');
 for (var i = 0; i < els.length; i++) {
   var el = els[i];
   var randomFilter = filters[Math.round(Math.random() * (filters.length - 1))];
-  el.className += (' ' + randomFilter);
+  el.classList.add(randomFilter);
 }
