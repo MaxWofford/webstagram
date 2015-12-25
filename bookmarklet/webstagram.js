80bc77b96391ba4235e9e4cf939c481759eabcc4
Max Wofford <max@maxwofford.com>
2015-12-24 23:20:00 -0800

    Filter entire page body

--- bookmarklet/webstagram.js
+++ bookmarklet/webstagram.js
@@ -3,9 +3,5 @@ var filters = ['ig-xpro2', 'ig-willow', 'ig-walden', 'ig-valencia',
 'ig-lofi', 'ig-kelvin', 'ig-inkwell', 'ig-hudson', 'ig-earlybird', 'ig-brannan',
 'ig-amaro', 'ig-1977'];
 
-var els = document.getElementsByTagName('img');
-for (var i = 0; i < els.length; i++) {
-  var el = els[i];
-  var randomFilter = filters[Math.round(Math.random() * (filters.length - 1))];
-  el.classList.add(randomFilter);
-}
+var randomFilter = filters[Math.round(Math.random() * (filters.length - 1))];
+document.body.classList.add(randomFilter);
