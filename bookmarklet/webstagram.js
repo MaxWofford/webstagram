25c6cad63f2c71abc4853d14de1590e009debafc
Max Wofford <max@maxwofford.com>
2015-12-25 00:04:57 -0800

    Fix filters from canceling eachother out

--- bookmarklet/webstagram.js
+++ bookmarklet/webstagram.js
@@ -3,5 +3,12 @@ var filters = ['ig-xpro2', 'ig-willow', 'ig-walden', 'ig-valencia',
 'ig-lofi', 'ig-kelvin', 'ig-inkwell', 'ig-hudson', 'ig-earlybird', 'ig-brannan',
 'ig-amaro', 'ig-1977'];
 
+// Get a random filter
 var randomFilter = filters[Math.round(Math.random() * (filters.length - 1))];
+
+// Remove all instagram filters applied to the body
+var igClassRegex = new RegExp("ig-.+?( |\$)", 'g');
+document.body.className = document.body.className.replace(igClassRegex, '');
+
+// Add our random filter to the body
 document.body.classList.add(randomFilter);
